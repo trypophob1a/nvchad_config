fc6fa47354518f7c6f063d03e936ebac9a33655b
trypophob1a <denial_of_service_attack@vk.com>
2024-03-22 19:04:22 +0500

    edit for android 12

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,69 +1,85 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        event = 'BufWritePre', -- uncomment for format on save
-        config = function()
-            require "configs.conform"
-        end,
-    },
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
 
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require("nvchad.configs.lspconfig").defaults()
-            require "configs.lspconfig"
-        end,
-    },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
 
-    {
-        "williamboman/mason.nvim",
-        opts = {
-            ensure_installed = {
-                "lua-language-server", "stylua",
-                "html-lsp", "css-lsp", "prettier", "gopls",
-                "gofumpt", "goimports", "goimports-reviser",
-                "golangci-lint", "golangci-lint-langserver",
-                "gotests", "gomodifytags",
-            },
-        },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "gopls",
+        "gofumpt",
+        "goimports",
+        --"goimports-reviser",
+        "golangci-lint",
+        "golangci-lint-langserver",
+        "gotests",
+        "gomodifytags",
+      },
     },
+  },
 
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = {
-            ensure_installed = {
-                "vim", "lua", "vimdoc",
-                "html", "css", "javascript", "go", "gomod", "gosum",
-                "gotmpl", "gowork",
-            },
-        },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "javascript",
+        "go",
+        "gomod",
+        "gosum",
+        "gotmpl",
+        "gowork",
+      },
     },
-    {
-        "Exafunction/codeium.vim",
-        event = 'BufEnter',
-    },
+  },
+  {
+    "Exafunction/codeium.vim",
+    event = "BufEnter",
+  },
 
-    {
-        "ray-x/go.nvim",
-        dependencies = {  -- optional packages
-            "ray-x/guihua.lua",
-        },
-        config = function()
-            require("go").setup()
-        end,
-        event = {"CmdlineEnter"},
-        ft = {"go", 'gomod'},
-        build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-    },
+  {
+    "ray-x/go.nvim",
+    -- dependencies = {  -- optional packages
+    --     "ray-x/guihua.lua",
+    -- },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
 
-    {
-        "kylechui/nvim-surround",
-        version = "*", -- Use for stability; omit to use `main` branch for the latest features
-        event = "VeryLazy",
-        config = function()
-            require("nvim-surround").setup({
-                -- Configuration here, or leave empty to use defaults
-            })
-        end
-    },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
 }
